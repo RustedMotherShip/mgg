b7279c156165a49bf3976411b9585f63eb72facb
RustedMotherShip <100175506+RustedMotherShip@users.noreply.github.com>
2025-02-06 16:08:41 +0300

    add temp

--- sway/mthrbar.sh
+++ sway/mthrbar.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-		echo "[   $(top -n 1 | grep 'CPU:' | awk '{print $2}') ][ $(free | grep Mem | awk '{printf "  %.1f%\n", $3/$2 * 100}') ][ $(date +"%X %Y-%m-%d") ][ $(hostname) ]"
+echo "[ 󰔄 $(sensors | grep 'Core 0' | awk '{print $3}') ][   $(top -n 1 | grep 'CPU:' | awk '{print $2}') ][ $(free | grep Mem | awk '{printf "  %.1f%\n", $3/$2 * 100}') ][ $(date +"%X %Y-%m-%d") ][ $(hostname) ]"
+#For Intel
+#echo "[ sensors | grep 'Tctl' | awk '{print $2}' ][   $(top -n 1 | grep 'CPU:' | awk '{print $2}') ][ $(free | grep Mem | awk '{printf "  %.1f%\n", $3/$2 * 100}') ][ $(date +"%X %Y-%m-%d") ][ $(hostname) ]"	
+#For AMD
